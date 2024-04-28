400d7c831c920027127469bb77142d2f903edd42
Sascha Böse <saschaboese1996@gmail.com>
2024-04-28 16:02:32 +0200

    remove attributes from pokemon card

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const DB_POKEMON_LIST_URL = 'https://pokeapi.co/api/v2/pokemon?limit=6&offset=0'
+const DB_POKEMON_LIST_URL = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0'
 const DB_POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon/';
 const DB_POKEMON_SPECIES_URL = 'https://pokeapi.co/api/v2/pokemon-species/'
 let currentPokemon;
@@ -34,9 +34,6 @@ function addInformationToPokemonCard(id) {
     document.getElementById('pokemonName'+id).innerHTML = currentPokemon.name;
     document.getElementById('element'+id).innerHTML = currentPokemon.types[0].type.name;
     document.getElementById('indexnumber'+id).innerHTML = '#'+currentPokemon.id;
-    document.getElementById('experience'+id).innerHTML = currentPokemon.base_experience;
-    document.getElementById('height'+id).innerHTML = currentPokemon.height;
-    document.getElementById('weight'+id).innerHTML = currentPokemon.weight;
     document.getElementById('image'+id).src = currentPokemon.sprites.other.home.front_default;
     document.getElementById('pokemoncard'+id).style.backgroundColor = currentPokemonID.color.name;
 }
