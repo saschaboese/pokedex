deed2c6c9fc40286dec7f1580f43470fa84367af
Sascha Böse <saschaboese1996@gmail.com>
2024-04-29 21:07:59 +0200

    add shadow style for pokemon img && add stats to pokemons array

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const DB_POKEMON_LIST_URL = 'https://pokeapi.co/api/v2/pokemon?limit=9&offset=0'
+let amountToLoadPokemonsFromAPI = 9;
+let startIndexToLoadPokemonsFromAPI = 0;
+const DB_POKEMON_LIST_URL = `https://pokeapi.co/api/v2/pokemon?limit=${amountToLoadPokemonsFromAPI}&offset=${startIndexToLoadPokemonsFromAPI}`;
 const DB_POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon/';
-const DB_POKEMON_SPECIES_URL = 'https://pokeapi.co/api/v2/pokemon-species/'
 let currentPokemon;
-let currentPokemonID;
-
 let listOfPokemons = [];
 let pokemons = [];
 
@@ -18,7 +17,7 @@ async function loadPokemons() {
 }
 
 async function savePokemonData() {
-    for (let i = 0; i < listOfPokemons.length; i++) {
+    for (let i = startIndexToLoadPokemonsFromAPI; i < listOfPokemons.length; i++) {
         const element = listOfPokemons[i];
         await getCurrentPokemonData(element.name);
         pushDataToArray_pokemons();
@@ -30,13 +29,16 @@ function pushDataToArray_pokemons() {
         'name': currentPokemon.name,
         'type': currentPokemon.types[0].type.name,
         'indexNumber': currentPokemon.id,
-        'image': currentPokemon.sprites.other.home.front_default
+        'image': currentPokemon.sprites.other.home.front_default,
+        'stats': {
+            'hp' : currentPokemon.stats[0].base_stat,
+            'attack' : currentPokemon.stats[1].base_stat,
+            'defense' : currentPokemon.stats[2].base_stat,
+            'special-attack' : currentPokemon.stats[3].base_stat,
+            'special-defense' : currentPokemon.stats[4].base_stat,
+            'speed' : currentPokemon.stats[5].base_stat
+        }
     })
-}
-
-async function getCurrentPokemonData(name) {
-    await fetchPokemon(name);
-    await fetchPokemonSpecies(name);
 }
 
 function renderPokemonCard() {
@@ -65,14 +67,13 @@ function changeBackgroundColor(pokemon, id) {
     document.getElementById('pokemoncard' + id).style.backgroundColor = CARD_COLOR[type];
 }
 
+async function getCurrentPokemonData(name) {
+    await fetchPokemon(name);
+}
+
 async function fetchPokemon(name) {
     let response = await fetch(DB_POKEMON_URL + name);
     currentPokemon = await response.json();
-}
-
-async function fetchPokemonSpecies(name) {
-    let response = await fetch(DB_POKEMON_SPECIES_URL + name);
-    currentPokemonID = await response.json();
 }
 
 async function fetchListOfPokemon() {
--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 function templateOf_pokemoncard(id) {
     return `
-    <div class="pokemon-card" id="pokemoncard${id}">
+    <div class="pokemon-card" id="pokemoncard${id}" onclick="showStats()">
     <div class="pokemon-indexnumber" id="indexnumber${id}">#25</div>
     <div class="pokemon-name" id="pokemonName${id}">Pikachu</div>
     <div class="pokemon-element" id="element${id}">Electric</div>
