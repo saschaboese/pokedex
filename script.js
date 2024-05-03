a2b718b709e779bba62bec8fdc3625be3e677279
Sascha Böse <saschaboese1996@gmail.com>
2024-05-03 11:07:41 +0200

    BugFixed: Loadmore function is now working again

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ let listOfPokemons = [];
 let pokemons = [];
 
 async function init() {
-    await loadPokemons();
-    renderPokemonCard();
+    await loadAndSavePokemonData(0,9);
+    renderPokemonCard(0,9);
 }
 
-async function loadPokemons() {
+async function loadAndSavePokemonData(startID,endID) {
     startLoadingIMG();
-    await getListOfPokemon();
-    await savePokemonData();
+    await getListOfPokemon(0,10000);
+    await savePokemonData(startID,endID);
     stopLoadingIMG();
 }
 
-async function savePokemonData() {
-    for (let i = startIndexToLoadPokemonsFromAPI; i < listOfPokemons.length; i++) {
-        const element = listOfPokemons[i];
+async function savePokemonData(startID,endID) {
+    for (startID; startID < endID; startID++) {
+        const element = listOfPokemons[startID];
         await getCurrentPokemonData(element.name);
         pushDataToArray_pokemons();
     }
@@ -33,20 +33,21 @@ function pushDataToArray_pokemons() {
         'indexNumber': currentPokemon.id,
         'image': currentPokemon.sprites.other.home.front_default,
         'stats': {
-            'hp' : currentPokemon.stats[0].base_stat,
-            'attack' : currentPokemon.stats[1].base_stat,
-            'defense' : currentPokemon.stats[2].base_stat,
-            'special-attack' : currentPokemon.stats[3].base_stat,
-            'special-defense' : currentPokemon.stats[4].base_stat,
-            'speed' : currentPokemon.stats[5].base_stat
+            'hp': currentPokemon.stats[0].base_stat,
+            'attack': currentPokemon.stats[1].base_stat,
+            'defense': currentPokemon.stats[2].base_stat,
+            'special-attack': currentPokemon.stats[3].base_stat,
+            'special-defense': currentPokemon.stats[4].base_stat,
+            'speed': currentPokemon.stats[5].base_stat
         }
     })
 }
 
-function renderPokemonCard() {
-    pokemons.forEach(pokemon => {
+function renderPokemonCard(startID,endID) {
+    for (startID; startID < endID; startID++) {
+        const pokemon = pokemons[startID];
         createPokemonCard(pokemon);
-    });
+    }
 }
 
 function createPokemonCard(pokemon) {
@@ -74,26 +75,27 @@ async function getCurrentPokemonData(name) {
     currentPokemon = await response.json();
 }
 
-async function getListOfPokemon() {
-    let response = await fetch(DB_POKEMON_LIST_URL);
+async function getListOfPokemon(startID,endID) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${endID}&offset=${startID}`);
     responseAsJson = await response.json();
     listOfPokemons = responseAsJson.results;
 }
 
 async function loadMore() {
-    startIndexToLoadPokemonsFromAPI = startIndexToLoadPokemonsFromAPI + 9;
-    endIndexToLoadPokemonsFromAPI = endIndexToLoadPokemonsFromAPI + 9;
-    DB_POKEMON_LIST_URL = `https://pokeapi.co/api/v2/pokemon?limit=${endIndexToLoadPokemonsFromAPI}&offset=${startIndexToLoadPokemonsFromAPI}`;
-    await loadPokemons();
-    renderPokemonCard();
+    let amountOfRenderdPokemons = document.querySelectorAll('.pokemon-card').length;
+    let endIdToRender = amountOfRenderdPokemons + 9;
+    startLoadingIMG();
+    await savePokemonData(amountOfRenderdPokemons,endIdToRender);
+    renderPokemonCard(amountOfRenderdPokemons,endIdToRender);
+    stopLoadingIMG();
 }
 
 function startLoadingIMG() {
-    document.getElementById('load').classList.replace('d-none','load');
+    document.getElementById('load').classList.replace('d-none', 'load');
 }
 
 function stopLoadingIMG() {
-    document.getElementById('load').classList.replace('load','d-none');
+    document.getElementById('load').classList.replace('load', 'd-none');
 }
 
 
@@ -101,7 +103,7 @@ function search() {
     let searchInput = document.getElementById('search').value;
     let findElement = pokemons.filter(item => item.name.toLowerCase().includes(searchInput.toLowerCase()));
     document.getElementById('content').innerHTML = '';
-    
+
     findElement.forEach(pokemon => {
         createPokemonCard(pokemon);
     });
